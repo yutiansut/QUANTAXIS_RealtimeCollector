24238f716022c84439dced27e78cdef5ab8bee02
yutiansut <yutiansut@qq.com>
2019-08-01 07:28:36 +0800

    #update for tdxExecutor

--- QARealtimeCollector/collectors/stockcollector.py
+++ QARealtimeCollector/collectors/stockcollector.py
@@ -1,4 +1,71 @@
-from QUANTAXIS.QAFetch.QATdx_adv import  QA_Tdx_Executor
+from QUANTAXIS.QAFetch.QATdx_adv import QA_Tdx_Executor
+from QUANTAXIS.QAEngine.QAThreadEngine import QA_Thread
+from QUANTAXIS.QAARP.QAUser import QA_User
 
-class QARealtimeCollector_Stock(QA_Tdx_Executor):
-    pass
+
+class QARealtimeCollector_Stock(QA_Thread):
+    def __init__(self, username, password):
+        super().__init__(name='QAREALTIME_COLLECTOR_STOCK')
+        self.user = QA_User(username=username, password=password)
+
+    def __getattr__(self, item):
+        try:
+            api = self.get_available()
+            func = api.__getattribute__(item)
+
+            def wrapper(*args, **kwargs):
+                res = self.executor.submit(func, *args, **kwargs)
+                self._queue.put(api)
+                return res
+            return wrapper
+        except:
+            return self.__getattr__(item)
+
+    def _queue_clean(self):
+        self._queue = queue.Queue(maxsize=200)
+
+    def _test_speed(self, ip, port=7709):
+
+        api = TdxHq_API(raise_exception=True, auto_retry=False)
+        _time = datetime.datetime.now()
+        # print(self.timeout)
+        try:
+            with api.connect(ip, port, time_out=1):
+                res = api.get_security_list(0, 1)
+                # print(res)
+                # print(len(res))
+                if len(api.get_security_list(0, 1)) > 800:
+                    return (datetime.datetime.now() - _time).total_seconds()
+                else:
+                    return datetime.timedelta(9, 9, 0).total_seconds()
+        except Exception as e:
+            return datetime.timedelta(9, 9, 0).total_seconds()
+
+    def get_market(self, code):
+        code = str(code)
+        if code[0] in ['5', '6', '9'] or code[:3] in ["009", "126", "110", "201", "202", "203", "204"]:
+            return 1
+        return 0
+
+    def subscriber(self, code):
+        """继续订阅
+
+        Arguments:
+            code {[type]} -- [description]
+        """
+        self.user.sub_code(code)
+
+    def get_realtime_concurrent(self, code):
+        code = [code] if isinstance(code, str) is str else code
+
+        try:
+            data = {self.get_security_quotes([(self.get_market(
+                x), x) for x in code[80 * pos:80 * (pos + 1)]]) for pos in range(int(len(code) / 80) + 1)}
+            return (pd.concat([self.api_no_connection.to_df(i.result()) for i in data]), datetime.datetime.now())
+        except:
+            pass
+
+    def get_data(self):
+
+    def run(self):
+        pass
