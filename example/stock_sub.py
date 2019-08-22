defe3f51af601e9cfda6e41e7cd574879f0965f1
yutiansut <yutiansut@qq.com>
2019-08-22 15:08:07 +0800

    #

--- example/stock_sub.py
+++ example/stock_sub.py
@@ -1,25 +1,42 @@
+import json
+
+import pandas as pd
+
 from QAPUBSUB.consumer import subscriber
 from QAPUBSUB.producer import publisher_routing
-import json
-import pandas as pd
-# 订阅操作
-
-req = publisher_routing(exchange='QARealtime_Market', routing_key='stock')
-
-req.pub(json.dumps({'topic': 'subscribe', 'code': '000007'}),routing_key='stock')
-
+from QUANTAXIS.QAEngine import QA_Thread, QA_Task
 
 # 接受并分析
 
 
-def callback(a, b, c, data):
-    data = json.loads(data)
+class client(QA_Thread):
 
-    print(pd.DataFrame(data).set_index(['code']).loc[:,['ask1', 'ask_vol1', 'price', 'bid1','bid_vol1']])
+    def __init__(self):
+        super().__init__(name='qasubclient', daemon=False)
 
+        self.req = publisher_routing(
+            exchange='QARealtime_Market', routing_key='stock')
+        self.last_ab = pd.DataFrame()
+        self.sub = subscriber(exchange='stocktransaction')
+        self.sub.callback = self.callback
 
-sub = subscriber(exchange='stocktransaction')
+    def subscribe(self, code='000007'):
+        req.pub(json.dumps({'topic': 'subscribe', 'code': code}),
+                routing_key='stock')
 
-sub.callback = callback
+    def callback(self, b, c, data):
+        data = json.loads(data)
 
-sub.start()
+        data = pd.DataFrame(data).set_index(['code']).loc[:, [
+            'ask_vol2', 'ask2', 'ask_vol1', 'ask1', 'price', 'bid1', 'bid_vol1', 'bid2', 'bid_vol2']]
+
+        self.put({'topic': 'new_data',
+                  'data': data})
+
+    def run(self):
+        threading.Thread(target=self.sub.start, daemon=True).start()
+        try:
+            data = self.queue.get_nowait()
+
+        except Exception as e:
+            print(e)
