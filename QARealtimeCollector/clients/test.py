11dfd20c3823c959231a479f2805ccdeda17fa7a
余天 <yutiansut@yutiandeMacBook-Pro.local>
2020-01-02 10:26:04 +0800

    #update

--- QARealtimeCollector/clients/test.py
+++ QARealtimeCollector/clients/test.py
@@ -1,5 +1,8 @@
 
 from QAPUBSUB.consumer import  subscriber
-sub = subscriber(host='192.168.2.116',user='admin', password='admin' ,exchange= 'realtime_60min_rb1910')
 
-sub.start()
+if __name__ == "__main__":
+
+    sub = subscriber(host='192.168.2.116',user='admin', password='admin' ,exchange= 'realtime_60min_rb1910')
+
+    sub.start()
